60e8140c20313f87c4625abb99237cd4892761a1
tleichtfried <tleichtfried2001@gmail.com>
2020-09-01 11:59:39 +0200

    Tokens

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,6 @@
 module Parser where
 
-data Token = Lambda | If | Set | POpen | PClose
+data Token = Lambda | If | Set | POpen | PClose | Identifier | Quote | ShortQuote 
+            | Num Number | String String | Complex Complex | Bool Bool | Char Char | Symbol String
+            | Point | QuasiQuote | ShortQuasiQuote | Unquote | ShortUnquote | UnquoteSplice 
+            | ShortUnquoteSplice | Label Integer | LabelRef Integer
