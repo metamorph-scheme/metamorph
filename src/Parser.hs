73e0e6841e57626fbdbd9c85d3a5476562cb999a
tleichtfried <tleichtfried2001@gmail.com>
2020-09-02 11:35:12 +0200

    Parser 0.2

--- src/Parser.hs
+++ src/Parser.hs
@@ -16,22 +16,36 @@ data MetaNode = LambdaNode [MetaNode] MetaNode MetaNode
             | IfNode MetaNode MetaNode MetaNode | SetNode MetaNode MetaNode | DefineNode MetaNode MetaNode 
             deriving Show 
 
+
+p1 = [POpen, Identifier "+", Integral 2, Integral 5, PClose]
+p2 = [POpen, POpen, Lambda,  POpen, Identifier "x", Identifier "y", Point, Identifier "z",PClose,
+    POpen, Identifier "/", Identifier "x", Identifier "y", PClose, PClose, Integral 3, Integral 9, Integral 4, String "sdf", PClose]
+p3 = [POpen, Quote, POpen, Identifier "*", Integral 2, Identifier "a",PClose, PClose]
+p4 = [ShortQuote, POpen, Identifier "*", Integral 2, POpen, Integral 3, Integral 5, PClose ,Identifier "a",PClose]
+p5 = [POpen, QuasiQuote, POpen, Identifier "*", Integral 4, POpen, Unquote, POpen, Identifier "+",
+    Real 3.4, Identifier "PI", PClose,PClose, Identifier "t", PClose, PClose]
+
+parseScheme :: [Token] -> MetaNode
+parseScheme st = case runState (parse "Scheme Program")  st of
+    (mn, []) -> mn
+    (_, t:_) -> error $ "Unexpected token " ++ show t ++  "not allowed in current context"
+
 push :: Token -> State [Token] ()
 push t = do
     ts <- get
     put (t:ts)
 
-pull :: State [Token] Token
-pull = do
+pull :: String -> State [Token] Token
+pull context = do
     ts <- get
     case ts of 
         (t:ts) -> do
             put ts
             return t
-        _ -> error "Unexpected end of token stream"
+        _ -> error $ "Unexpected end of token stream in " ++ context
 
-pullEq :: Token -> State [Token] ()
-pullEq c = do
+pullEq :: String -> Token -> State [Token] ()
+pullEq context c = do
     ts <- get
     case ts of
         (t:ts) -> do
@@ -39,27 +53,33 @@ pullEq c = do
             if  t ==  c then
                 return ()
             else
-                error $ (show c) ++ " expected, but not found"
-        _ -> error "Unexpected end of token stream"
+                error $ (show c) ++ "in " ++ context ++ " expected, but not found"
+        _ -> error $ "Unexpected end of token stream in " ++ context
 
-parse :: State [Token] MetaNode
-parse = do
-    t <- pull
+peek :: String -> State [Token] Token
+peek context = do
+    ts <- get
+    case ts of
+        (t:ts) -> return t 
+        _ -> error $ "Unexpected end of token stream in " ++ context 
+
+parse :: String -> State [Token] MetaNode
+parse context = do
+    t <- peek context
     case t of
-        POpen -> parseExpr
-        ShortQuote -> parseQuotedDatum
-        ShortQuasiQuote -> parseQuasiQuotedDatum
-        Bool b -> return $ BoolAtom b
-        String s -> return $ StringAtom s
-        Real n -> return $ RealAtom n
-        Integral n -> return $ IntegralAtom n
-        Rational n m -> return $ RationalAtom n m
-        Complex c -> return $ ComplexAtom c
-        Identifier i -> return $ IdentifierAtom i
+        POpen -> parseSyntax
+        ShortQuote -> do
+            pullEq "Datum" ShortQuote
+            parseDatum
+        ShortQuasiQuote -> do
+            pullEq "Quasiquoted Datum" ShortQuasiQuote
+            parseQuasiQuotedDatum
+        _ -> parseAtom
 
-parseExpr :: State [Token] MetaNode
-parseExpr = do 
-    t <- pull
+parseSyntax :: State [Token] MetaNode
+parseSyntax = do
+    pullEq "Syntactic construct" POpen 
+    t <- peek "Syntactic construct"
     case t of
         Lambda -> parseLambda
         Define -> parseDefine
@@ -67,187 +87,184 @@ parseExpr = do
         QuasiQuote -> parseQuasiQuote
         If -> parseIf
         Set -> parseSet
-        _ -> do
-            push t
-            parseApplication
+        _ -> parseApplication
+
+parseAtom :: State [Token] MetaNode
+parseAtom = do 
+    t <- pull "Atom"
+    case t of  
+        Bool b -> return $ BoolAtom b
+        String s -> return $ StringAtom s
+        Real n -> return $ RealAtom n
+        Integral n -> return $ IntegralAtom n
+        Rational n m -> return $ RationalAtom n m
+        Complex c -> return $ ComplexAtom c
+        Identifier i -> return $ IdentifierAtom i
+        Quote -> return $ IdentifierAtom "Quote"
+        Unquote -> return $ IdentifierAtom "Unquote"
+        QuasiQuote -> return $ IdentifierAtom "Quasiquote"
+        _ -> error $ "Unexpected Token " ++ show t ++ " not allowed in current context"
 
 parseQuote :: State [Token] MetaNode
 parseQuote = do
-    d <- parseQuotedDatum
-    pullEq PClose
+    pullEq "Quote" Quote
+    d <- parseDatum
+    pullEq "Quote" PClose
     return d
 
 parseQuasiQuote :: State [Token] MetaNode
 parseQuasiQuote = do
+    pullEq "Quasiquote" QuasiQuote
     d <- parseQuasiQuotedDatum
-    pullEq PClose
+    pullEq "Quasiquote" PClose
     return d
 
 parseLambda :: State [Token] MetaNode
 parseLambda = do
+    pullEq "Lambda" Lambda
     (c, l) <- parseParams
-    e <- parse
-    pullEq PClose
+    e <- parse "Lambda Body"
+    pullEq "Lambda" PClose
     return (LambdaNode c l e) 
 
 parseIf :: State [Token] MetaNode
 parseIf = do
-    p <- parse 
-    a <- parse 
-    b <- parse
-    pullEq PClose
+    pullEq "If" If
+    p <- parse "If Condition"
+    a <- parse "If Then Branch"
+    b <- parse "If Else Branch"
+    pullEq "If" PClose
     return (IfNode p a b)  
 
 parseSet :: State [Token] MetaNode
 parseSet = do
-    t <- pull
+    pullEq "Set" Set
+    t <- pull "Set"
     case t of 
         (Identifier str) -> do
-            e <- parse
+            e <- parse "Set Body"
             return (SetNode (IdentifierAtom str) e)
-        _ -> error "Expected Identifier"
+        _ -> error "Expected Identifier as first argument of set"
 
 parseDefine :: State [Token] MetaNode
 parseDefine = do
-    t <- pull
+    pullEq "Define" Define
+    t <- pull "Define"
     case t of 
         (Identifier str) -> do
-            e <- parse
+            e <- parse "Define Body"
             return (DefineNode (IdentifierAtom str) e)
-        _ -> error "Expected Identifier"
+        _ -> error "Expected Identifier as first argument of define"
 
 
 parseApplication :: State [Token] MetaNode
 parseApplication = do
-    f <- parse
+    f <- parse "Application"
     arg <- parseList
     return (ApplicationNode f arg)
 
-parseQuotedDatum :: State [Token] MetaNode
-parseQuotedDatum = do
-    t <- pull
+parseDatum :: State [Token] MetaNode
+parseDatum = do
+    t <- peek "Datum"
     case t of        
-        POpen -> ListNode <$> parseListQuotedDatum
-        ShortQuote -> ListNode <$> ((\x -> [(IdentifierAtom "Quote"),x]) <$> parseQuotedDatum)
-        ShortQuasiQuote -> ListNode <$> ((\x -> [(IdentifierAtom "Quasiquote"),x]) <$> parseQuotedDatum)
-        ShortUnquote -> ListNode <$> ((\x -> [(IdentifierAtom "Unquote"),x]) <$> parseQuotedDatum)
-        Quote -> return $ IdentifierAtom "Quote"
-        Unquote -> return $ IdentifierAtom "Unquote"
-        QuasiQuote -> return $ IdentifierAtom "Quasiquote"
-        Bool b -> return $ BoolAtom b
-        String s -> return $ StringAtom s
-        Real n -> return $ RealAtom n
-        Integral n -> return $ IntegralAtom n
-        Rational n m -> return $ RationalAtom n m
-        Complex c -> return $ ComplexAtom c
-        Identifier i -> return $ IdentifierAtom i
+        POpen -> do
+            pullEq "Compound Datum" POpen
+            ListNode <$> parseCompoundDatum
+        ShortQuote -> parseQuotedShortForm
+        ShortUnquote -> parseQuotedShortForm
+        ShortQuasiQuote -> parseQuotedShortForm
+        _ -> parseAtom
+
+parseCompoundDatum :: State [Token] [MetaNode]
+parseCompoundDatum = do
+    t <- peek "Compound Datum"
+    case t of 
+        PClose -> do
+            pullEq "Compound Datum" PClose
+            return []
+        _ -> do
+            e <- parseDatum
+            es <- parseCompoundDatum
+            return (e:es)
+
+parseQuotedShortForm :: State [Token] MetaNode
+parseQuotedShortForm = do
+    t <- pull "Quoted Shortform"
+    ListNode <$> ((\x -> [(IdentifierAtom (show t)),x]) <$> parseDatum)
 
 parseQuasiQuotedDatum :: State [Token] MetaNode
 parseQuasiQuotedDatum = do
-    t <- pull
+    t <- peek "Quasiquoted Datum"
     case t of        
         POpen -> do
-            t <- pull
+            pullEq "Quasiquoted Datum" POpen
+            t <- peek "Quasiquoted Datum"
             case t of
                 Unquote -> do
-                    e <- parse
-                    pullEq PClose
+                    pullEq "Unquoted Expression" Unquote 
+                    e <- parse "Unquoted Expression"
+                    pullEq "Unquoted Expression" PClose
                     return e
-                _ -> do
-                    push t
-                    ListNode <$> parseListQuasiQuotedDatum
-        ShortQuote -> ListNode <$> ((\x -> [(IdentifierAtom "Quote"),x]) <$> parseQuasiQuotedDatum)
-        ShortQuasiQuote -> ListNode <$> ((\x -> [(IdentifierAtom "Quasiquote"),x]) <$> parseQuasiQuotedDatum)
-        ShortUnquote -> parse
-        Quote -> return $ IdentifierAtom "Quote"
-        Unquote -> return $ IdentifierAtom "Unquote"
-        QuasiQuote -> return $ IdentifierAtom "Quasiquote"
-        Bool b -> return $ BoolAtom b
-        String s -> return $ StringAtom s
-        Real n -> return $ RealAtom n
-        Integral n -> return $ IntegralAtom n
-        Rational n m -> return $ RationalAtom n m
-        Complex c -> return $ ComplexAtom c
-        Identifier i -> return $ IdentifierAtom i
+                _ -> ListNode <$> parseQuasiQuotedCompoundDatum
+        ShortUnquote -> do
+            pullEq "Unquoted Expression" ShortUnquote
+            parse "Unquoted Expression"
+        ShortQuote -> parseQuasiQuotedShortForm
+        ShortQuasiQuote -> parseQuasiQuotedShortForm
+        _ -> parseAtom
 
-parseListQuasiQuotedDatum :: State [Token] [MetaNode]
-parseListQuasiQuotedDatum = do
-    ts <- get
-    case ts of 
-        (t:ts) ->
-            case t of 
-                PClose -> do
-                    pull
-                    return []
-                _ -> do
-                    e <- parseQuasiQuotedDatum
-                    es <- parseListQuasiQuotedDatum
-                    return (e:es)
-        _ -> error "Unexpected end of tokenstream"
+parseQuasiQuotedCompoundDatum :: State [Token] [MetaNode]
+parseQuasiQuotedCompoundDatum = do
+    t <- peek "Quasiquoted Compound Datum"
+    case t of 
+        PClose -> do
+            pullEq "Quasiquoted Compound Datum" PClose
+            return []
+        _ -> do
+            e <- parseQuasiQuotedDatum
+            es <- parseQuasiQuotedCompoundDatum
+            return (e:es)
 
-parseListQuotedDatum :: State [Token] [MetaNode]
-parseListQuotedDatum = do
-    ts <- get
-    case ts of 
-        (t:ts) ->
-            case t of 
-                PClose -> do
-                    pull
-                    return []
-                _ -> do
-                    e <- parseQuotedDatum
-                    es <- parseListQuotedDatum
-                    return (e:es)
-        _ -> error "Unexpected end of tokenstream"
+parseQuasiQuotedShortForm :: State [Token] MetaNode
+parseQuasiQuotedShortForm = do
+    t <- pull "Quasiquoted Shortform"
+    ListNode <$> ((\x -> [(IdentifierAtom (show t)),x]) <$> parseQuasiQuotedDatum)
 
 parseList :: State [Token] [MetaNode]
 parseList = do
-    ts <- get
-    case ts of 
-        (t:ts) ->
-            case t of 
-                PClose -> do
-                    pull
-                    return []
-                _ -> do
-                    e <- parse
-                    es <- parseList
-                    return (e:es)
-        _ -> error "Unexpected end of tokenstream"
+    t <- peek "List"
+    case t of 
+        PClose -> do
+            pullEq "List" PClose
+            return []
+        _ -> do
+            e <- parse "List Element"
+            es <- parseList
+            return (e:es)
 
 parseParams :: State [Token] ([MetaNode], MetaNode)
 parseParams = do
-    t <- pull
+    t <- pull "Formal Paramters"
     case t of
         POpen -> parseIdentifierList
         Identifier s -> return ([],IdentifierAtom s)  
-        _ -> error "Expected parameter list or single parameter"
+        _ -> error "Expected parameter list or single parameter in lambda definition"
 
 parseIdentifierList :: State [Token] ([MetaNode], MetaNode)
 parseIdentifierList = do 
-    ts <- get
-    case ts of
-        (t:ts) ->
+    t <- pull "Formal Paramters"
+    case t of 
+        PClose -> do
+            return ([], IdentifierAtom "")
+        Point -> do
+            t <- pull "Formal Paramters"
             case t of 
-                PClose -> do
-                    pull
-                    return ([], IdentifierAtom "")
-                Point -> do
-                    pull
-                    ts <- get
-                    case ts of
-                        (t:ts) ->
-                            case t of 
-                                Identifier str -> do
-                                    pull
-                                    pullEq PClose
-                                    return ([], IdentifierAtom str)
-                                _ -> error "In a lambda parameter list are only identifiers allowed"
-                        _ -> error "Unexpected end of tokenstream"    
                 Identifier str -> do
-                    pull
-                    (is, i) <- parseIdentifierList
-                    return ((IdentifierAtom str):is, i)
-                _ -> error "In a lambda parameter list are only identifiers allowed"
-        _ -> error "Unexpected end of token stream"
+                    pullEq "Formal Paramters" PClose
+                    return ([], IdentifierAtom str)
+                _ -> error $ "Expected parameter list or single parameter in lambda definition, not token " ++ show t
+        Identifier str -> do
+            (is, i) <- parseIdentifierList
+            return ((IdentifierAtom str):is, i)
+        _ -> error $ "Expected parameter list or single parameter in lambda definition, not token " ++ show t
                 
