a2b2b50d540e8e0d65b77e60518895402d0e99b8
rgruber <raphael.gruber@gmail.com>
2021-03-14 18:54:47 +0100

    fix duplicated paths, add continuations and if, some refactors

---
+++ testdata/simple_if.scm
@@ -0,0 +1,7 @@
+(define my-cond #t)
+(define out (current-output-port))
+
+(let ((other-cond #t))
+  (if (and my-cond other-cond (and (or #t #f #f #f #f other-cond) #t #t (if #t my-cond 8) #t)) 
+    "YES"
+    "NO"))
